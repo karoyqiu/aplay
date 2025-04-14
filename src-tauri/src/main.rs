c4e5388dfea109ded9261f5ff76bd9b3c3a2a7f2
Roy QIU <karoyqiu@gmail.com>
2025-04-14 11:21:56 +0800

    调整窗口。

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    aplay_lib::run()
+  aplay_lib::run()
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { getCurrentWindow } from '@tauri-apps/api/window';
+
 import '@/App.css';
 import { Button } from '@/components/ui/button';
 
+const appWindow = getCurrentWindow();
+
 function App() {
-  return <Button>This is a button</Button>;
+  return (
+    <div
+      className="bg-primary/30 h-screen w-screen"
+      onMouseDown={(e) => {
+        if (e.currentTarget === e.target) {
+          appWindow.startDragging();
+        }
+      }}
+    >
+      <Button>This is a button</Button>
+    </div>
+  );
 }
 
 export default App;
