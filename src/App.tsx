2e03c508df5f79045195a78bfde5a29f9f7ff649
Roy QIU <karoyqiu@gmail.com>
2025-04-14 13:39:50 +0800

    添加字幕显示。

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { convertFileSrc } from '@tauri-apps/api/core';
 import { getCurrentWindow } from '@tauri-apps/api/window';
 import { open } from '@tauri-apps/plugin-dialog';
-import { FolderOpen, LogOut, Play } from 'lucide-react';
-import { useRef } from 'react';
+import { FolderOpen, LogOut, Pause, Play } from 'lucide-react';
+import { useRef, useState } from 'react';
+import { useEventCallback } from 'usehooks-ts';
 
 import '@/App.css';
 import { Button } from '@/components/ui/button';
@@ -10,40 +12,136 @@ import { Slider } from '@/components/ui/slider';
 
 const appWindow = getCurrentWindow();
 
+const pad = (value: number, maxLength = 2) => value.toString().padStart(maxLength, '0');
+
+const nts = (seconds: number) => {
+  const s = Math.floor(seconds % 60);
+  const m = Math.floor(seconds / 60) % 60;
+  const h = Math.floor(seconds / 60 / 60);
+  return `${pad(h)}:${pad(m)}:${pad(s)}`;
+};
+
 function App() {
+  const [canPlay, setCanPlay] = useState(false);
+  const [playing, setPlaying] = useState(false);
+  const [duration, setDuration] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [seeking, setSeeking] = useState(false);
   const player = useRef<HTMLAudioElement>(null);
+  const track = useRef<HTMLTrackElement>(null);
+  const [cues, setCues] = useState<string[]>([]);
+
+  const onCueChange = useEventCallback(() => {
+    if (track.current) {
+      const actives = track.current.track.activeCues;
+      const lines: string[] = [];
+
+      if (actives) {
+        for (const cue of actives) {
+          if (cue instanceof VTTCue) {
+            lines.push(cue.text);
+          }
+        }
+      }
+
+      setCues(lines);
+    }
+  });
 
   return (
     <div
-      className="bg-primary/30 h-screen w-screen p-2"
+      className="bg-primary/20 flex h-screen w-screen flex-col p-2"
       onMouseDown={(e) => {
         if (e.currentTarget === e.target) {
           appWindow.startDragging();
         }
       }}
     >
+      <div className="my-auto flex flex-col items-center text-xl">
+        {cues.map((cue) => (
+          <p key={cue}>{cue}</p>
+        ))}
+      </div>
       <div className="flex items-center gap-2">
         <ButtonGroup variant="outline" size="icon">
           <Button
             onClick={async () => {
-              const filename = await open({
-                filters: [{ extensions: ['mp3', 'wav'], name: 'Audio files' }],
-              });
+              if (player.current && track.current) {
+                track.current.oncuechange = onCueChange;
+
+                const filename = await open({
+                  filters: [
+                    { extensions: ['mp3', 'wav'], name: 'Audio files' },
+                    { extensions: ['vtt'], name: 'WebVTT files' },
+                  ],
+                });
+
+                if (filename) {
+                  if (filename.endsWith('.vtt')) {
+                    track.current.src = convertFileSrc(filename);
+                  } else {
+                    player.current.src = convertFileSrc(filename);
+                    track.current.src = `${player.current.src}.vtt`;
+                    await player.current.play();
+                  }
+                }
+              }
             }}
           >
             <FolderOpen />
           </Button>
-          <Button>
-            <Play />
+          <Button
+            disabled={!canPlay}
+            onClick={async () => {
+              if (playing) {
+                await player.current?.pause();
+              } else {
+                await player.current?.play();
+              }
+            }}
+          >
+            {playing ? <Pause /> : <Play />}
           </Button>
         </ButtonGroup>
-        <span className="shrink-0 font-mono text-sm">0:00 / 0:00</span>
-        <Slider />
+        <span className="shrink-0 font-mono text-sm">
+          {`${nts(currentTime)} / ${nts(duration)}`}
+        </span>
+        <Slider
+          max={duration || 1}
+          value={[currentTime]}
+          disabled={!canPlay}
+          onValueChange={([value]) => {
+            setSeeking(true);
+            setCurrentTime(value);
+          }}
+          onValueCommit={([value]) => {
+            if (player.current) {
+              player.current.currentTime = value;
+            }
+
+            setSeeking(false);
+          }}
+        />
         <Button variant="destructive" size="icon" onClick={() => appWindow.close()}>
           <LogOut />
         </Button>
       </div>
-      <audio className="hidden" ref={player} />
+      <audio
+        className="hidden"
+        ref={player}
+        onCanPlay={() => setCanPlay(true)}
+        onPlay={() => setPlaying(true)}
+        onPause={() => setPlaying(false)}
+        onDurationChange={(e) => setDuration(e.currentTarget.duration)}
+        onTimeUpdate={(e) => {
+          if (!seeking) {
+            setCurrentTime(e.currentTarget.currentTime);
+          }
+        }}
+        crossOrigin="anonymous"
+      >
+        <track default ref={track} kind="subtitles" srcLang="zh" />
+      </audio>
     </div>
   );
 }
